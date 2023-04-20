0043fe84aa7c6bb7eede93eca28abc80595c3adc
ehsan <ehsansepahi277@gmail.com>
2023-04-20 19:26:19 +0430

    add json req

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OpenAIController extends Controller
 {
-    //
+
+    public function index(): Jsonable
+    {
+        $search = "laravel get ip address";
+
+        $data = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
+        ])
+            ->post("https://api.openai.com/v1/chat/completions", [
+                "model" => "gpt-3.5-turbo",
+                'messages' => [
+                    [
+                        "role" => "user",
+                        "content" => $search
+                    ]
+                ],
+                'temperature' => 0.5,
+                "max_tokens" => 200,
+                "top_p" => 1.0,
+                "frequency_penalty" => 0.52,
+                "presence_penalty" => 0.5,
+                "stop" => ["11."],
+            ])
+            ->json();
+
+        return response()->json($data['choices'][0]['message'], 200, array(), JSON_PRETTY_PRINT);
+    }
+
 }
